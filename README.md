180d60bad8dc6f49611b748e099be14c3867f195
Phil Martinez <phil@philsquare.com>
2013-12-25 13:44:43 -0600

    First commit for the Swift theme

---
+++ README.md
@@ -0,0 +1,2 @@
+## Overview
+Swift is a clean and simple PyroCMS theme designed to make it easy to get a website up and going ... well ... swiftly.
---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Swift extends Theme {
+
+    public $name = 'Swift';
+    public $author = 'Philsquare';
+    public $author_website = 'http://philsquare.com';
+    public $website = 'http://philsquare.com';
+    public $description = 'Get started swiftly with this simple & clean PyroCMS theme';
+    public $version = '1.0.0';
+
+}
+
+/* End of file theme.php */
